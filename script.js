c4c17a1fba0262403f9ec78dfa69ef091f89cde7
Claudia2305 <claudia2305c@hotmail.com>
2025-03-27 11:00:31 +0100

    parser til...

--- script.js
+++ script.js
@@ -3,11 +3,21 @@
 // Steg 2: Lag en variabel som bruker await for å ta i mot en array med objects
 // Steg 3: Retuner den variabelen
 
-// OPPGAVE 2:
+
+async function fetchData() {
+    return "Data er hentet!";
+  }
+
+ // OPPGAVE 2:
 // Steg 1: Lag en async funskjon som heter dataRecieved
 // Steg 2: Lag en variabel som bruker await for å ta i mot funksjonen getData
 // Steg 3: Logg ut denne dataen
 // Steg 4: Legg til denne dataen på siden
+
+async function dataReceived() {
+    const data = await getData();
+    console.log(data);
+  }
 
 //
 //
@@ -16,9 +26,16 @@
 
 // API-link: https://catfact.ninja/facts
 
+async function fetchApi(){
 // Fetcher data fra catFact
+const data = await fetch('https://catfact.ninja/facts');
 
-// Parser til JSON
+// Parser til JSON til array
+}
+
+fetchApi();
+
+
 // Hele arrayet
 // Vi finner data array, array med objects
 // Vi velger et spesifikt array
